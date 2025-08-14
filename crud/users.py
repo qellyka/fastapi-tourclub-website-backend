dcf10ae4adf9ea21101e91f3d76f1d1989de0150
qellyka <qellyka@proton.me>
2025-08-14 19:03:06 +0300

    Add crud operations for user

---
+++ crud/users.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models import UserModel
+from sqlalchemy import select, or_
+
+from schemas import RegisterUser
+
+
+async def get_user_by_email_or_username(
+    session: AsyncSession,
+    username: str,
+    email: str,
+) -> Optional[UserModel]:
+    db_user = await session.scalar(
+        select(UserModel).where(
+            or_(UserModel.username == username, UserModel.email == email)
+        )
+    )
+    return db_user
+
+
+async def create_new_user(
+    session: AsyncSession,
+    user: RegisterUser,
+    hashed_password: str,
+) -> UserModel:
+    new_user = UserModel(
+        username=user.username,
+        email=user.email,
+        password=hashed_password,
+        first_name=user.first_name.title(),
+        last_name=user.last_name.title(),
+        middle_name=user.middle_name.title() if user.middle_name else None,
+    )
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
+
+
+async def activate_user(session: AsyncSession, username: str):
+    user = await session.scalar(select(UserModel).where(UserModel.username == username))
+    if not user:
+        return None
+    user.is_activated = True
+    await session.commit()
+    return user
