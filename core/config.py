6a2bfb244da9cb128d31e76bce50a79ad1f8d6c0
qellyka <qellyka@proton.me>
2025-08-14 19:04:02 +0300

    Add config file

---
+++ core/config.py
@@ -0,0 +1,34 @@
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # --- App settings ---
+    DEBUG: bool = False
+
+    # --- Database ---
+    DATABASE_URL: str
+
+    # --- JWT settings ---
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRES_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRES_DAYS: int = 7
+    VERIFY_TOKEN_EXPIRES_HOURS: int = 48
+
+    # --- Email settings ---
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: EmailStr
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+
+    class Config:
+        env_file = "app/.env"
+        env_file_encoding = "utf-8"
+
+
+# Экземпляр настроек
+settings = Settings()
