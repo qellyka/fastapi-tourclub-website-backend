fae8886b79086db1e0a060bcbc834d38fe851778
qellyka <qellyka@proton.me>
2025-08-14 19:03:27 +0300

    Add token models

---
+++ models/tokens.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+
+from .base import Base
+
+
+class TokenModel(AsyncAttrs, Base):
+    __tablename__ = "tokens"
+
+    token: Mapped[str] = mapped_column(unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
