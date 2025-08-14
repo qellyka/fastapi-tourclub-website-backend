2ee4da7550fd626284b3f7e92f22fa989d16d832
qellyka <qellyka@proton.me>
2025-08-14 22:12:39 +0300

    Add lifespan function

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
+from api import user_router
+from db import db_helper
+from models import Base
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(user_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main::app", reload=True)
+    uvicorn.run("main:app", reload=True)
