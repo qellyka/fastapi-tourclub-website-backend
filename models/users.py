40faca646ec0868264312331765c1e8156286a0a
qellyka <qellyka@proton.me>
2025-08-14 22:12:22 +0300

    Added new columns: hikes_participations, club_participant and new tables: HikeParticipant, ClubParticipant

--- models/users.py
+++ models/users.py
@@ -2,10 +2,11 @@ from datetime import datetime
 from typing import Optional, List
 
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean, ARRAY
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ARRAY, ForeignKey, Integer, Text
 from sqlalchemy.sql import func
 from sqlalchemy.types import TIMESTAMP
+from .hikes import HikeModel
 
 from .base import Base
 
@@ -13,19 +14,21 @@ from .base import Base
 class UserModel(AsyncAttrs, Base):
     __tablename__ = "users"
 
-    username: Mapped[str] = mapped_column(
-        String(32), unique=True, nullable=False, index=True
-    )
-    email: Mapped[str] = mapped_column(
-        String(256), unique=True, nullable=False, index=True
-    )
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
-
     first_name: Mapped[str] = mapped_column(String(64), nullable=False)
     last_name: Mapped[str] = mapped_column(String(64), nullable=False)
     middle_name: Mapped[Optional[str]] = mapped_column(String(64), nullable=True)
+    is_activated: Mapped[bool] = mapped_column(default=False)
+    roles: Mapped[List[str]] = mapped_column(ARRAY(String), default=["guest"])
 
-    is_activated: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    hikes_participations: Mapped[Optional[List["HikeParticipant"]]] = relationship(
+        back_populates="user", default=[]
+    )
+    club_participant: Mapped[Optional["ClubParticipant"]] = relationship(
+        back_populates="user", uselist=False
+    )
 
     created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
@@ -39,4 +42,24 @@ class UserModel(AsyncAttrs, Base):
         nullable=False,
     )
 
-    roles: Mapped[List[str]] = mapped_column(ARRAY(String), default=["guest"])
+
+class HikeParticipant(Base):
+    __tablename__ = "hike_participants"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    hike_id: Mapped[int] = mapped_column(ForeignKey("hikes.id"))
+    role: Mapped[str] = mapped_column(String)
+
+    user: Mapped["UserModel"] = relationship(back_populates="hikes_participations")
+    hike: Mapped["HikeModel"] = relationship(back_populates="participants_info")
+
+
+class ClubParticipant(Base):
+    __tablename__ = "club_participants"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    description: Mapped[Optional[str]] = mapped_column(Text)
+
+    user: Mapped["UserModel"] = relationship(back_populates="club_participant")
