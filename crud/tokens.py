21772c5eef8acf411dc99527a96c4dbe69c1171e
qellyka <qellyka@proton.me>
2025-08-14 19:03:14 +0300

    Add crud operations for token

---
+++ crud/tokens.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models import UserModel
+from sqlalchemy import select, or_, delete
+
+from models.tokens import TokenModel
+
+
+async def save_token(
+    session: AsyncSession,
+    refresh_token: str,
+    user_id: int,
+):
+    token_data = await session.scalar(
+        select(TokenModel).where(TokenModel.user_id == user_id)
+    )
+    if token_data:
+        TokenModel.token = refresh_token
+        await session.commit()
+    new_token = TokenModel(
+        token=refresh_token,
+        user_id=user_id,
+    )
+    session.add(new_token)
+    await session.commit()
+    return new_token
+
+
+async def remove_token(
+    token: str,
+    session: AsyncSession,
+):
+    stmt = delete(TokenModel).where(TokenModel.token == token)
+    await session.execute(stmt)
+    await session.commit()
+
+
+async def find_token(
+    token: str,
+    session: AsyncSession,
+):
+    token_data = await session.scalar(
+        select(TokenModel).where(TokenModel.token == token)
+    )
+    return token_data
