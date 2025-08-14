8aef5ce3e474763165f87ba27d13dd6423231d5c
qellyka <qellyka@proton.me>
2025-08-14 19:02:53 +0300

    Add some utils for auth

---
+++ schemas/users.py
@@ -0,0 +1,89 @@
+from pydantic import BaseModel, EmailStr, Field, computed_field
+
+
+class UserBase(BaseModel):
+    username: str = Field(
+        ...,
+        min_length=3,
+        max_length=32,
+        pattern=r"^[a-zA-Z0-9_-]+$",
+        description="Латиница, цифры, _ и -",
+    )
+    email: EmailStr
+    first_name: str = Field(
+        ...,
+        min_length=2,
+        max_length=64,
+        pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+        description="Имя без цифр и спецсимволов",
+    )
+    last_name: str = Field(
+        ...,
+        min_length=2,
+        max_length=64,
+        pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+        description="Фамилия без цифр и спецсимволов",
+    )
+    middle_name: str | None = Field(
+        None,
+        min_length=2,
+        max_length=64,
+        pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+        description="Отчество без цифр и спецсимволов",
+    )
+
+    @computed_field(return_type=str)
+    @property
+    def full_name(self) -> str:
+        parts = [
+            self.last_name.title(),
+            self.first_name.title(),
+            self.middle_name.title() if self.middle_name else "",
+        ]
+        return " ".join(filter(None, parts))
+
+
+class RegisterUser(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=128,
+        description="Пароль от 8 до 128 символов",
+    )
+
+
+class UserRead(UserBase):
+    id: int
+    is_activated: bool
+    model_config = {"from_attributes": True}
+
+
+class UserUpdate(BaseModel):
+    first_name: str | None = Field(
+        None,
+        min_length=2,
+        max_length=64,
+        pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+    )
+    last_name: str | None = Field(
+        None,
+        min_length=2,
+        max_length=64,
+        pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+    )
+    middle_name: str | None = Field(
+        None,
+        min_length=2,
+        max_length=64,
+        pattern=r"^[a-zA-Zа-яА-ЯёЁ-]+$",
+    )
+
+
+class LoginUser(BaseModel):
+    username: str
+    password: str
+
+
+class UserCreateResponse(BaseModel):
+    detail: dict
+    user: UserRead
