56f0e7d5111a572f43bea819357cea706727f36a
qellyka <qellyka@proton.me>
2025-08-15 18:28:07 +0300

    Update hike schemas

--- schemas/hikes.py
+++ schemas/hikes.py
@@ -1,5 +1,5 @@
 from typing import Any
-from datetime import datetime
+from datetime import date
 from pydantic import BaseModel, Field
 
 
@@ -7,11 +7,11 @@ class HikeBase(BaseModel):
     name: str = Field(..., description="Название похода")
     complexity: str = Field(..., description="Сложность маршрута")
     route: str = Field(..., description="Маршрут похода")
-    geojson_data: dict[str, Any] = Field(
-        ..., description="Данные маршрута в формате GeoJSON"
-    )
-    start_date: datetime = Field(..., description="Дата начала похода")
-    end_date: datetime = Field(..., description="Дата окончания похода")
+    # geojson_data: dict[str, Any] = Field(
+    #     ..., description="Данные маршрута в формате GeoJSON"
+    # )
+    start_date: date = Field(..., description="Дата начала похода")
+    end_date: date = Field(..., description="Дата окончания похода")
     region: str = Field(..., description="Регион проведения похода")
     description: str = Field(..., description="Описание похода")
     photos_archive: str = Field(..., description="Ссылка на архив с фотографиями")
