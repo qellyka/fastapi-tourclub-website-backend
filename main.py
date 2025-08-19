3861556357d3390655426494e8c970c0e658a8ea
qellyka <qellyka@proton.me>
2025-08-19 14:09:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from api import user_router
+from api import user_router, hike_router, pass_router, additional_router
 from core.config import settings
 from db import db_helper
 from models import Base
-from models import UserModel, HikeParticipant, ClubParticipant
-from models import HikeModel
-from models import PassModel
-from models import TokenModel
+from models import (
+    UserModel,
+    HikeParticipant,
+    ClubParticipant,
+    HikeModel,
+    PassModel,
+    TokenModel,
+)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
 
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(user_router)
+app.include_router(hike_router)
+app.include_router(pass_router)
+app.include_router(additional_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, host=settings.HOST, port=settings.PORT)
