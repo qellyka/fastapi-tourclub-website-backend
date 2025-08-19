d9b49e12a1fafd1bcc04add2c78b545e76e2c55b
qellyka <qellyka@proton.me>
2025-08-19 14:07:36 +0300

    Add additional crud operations

---
+++ crud/additional.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from models import HikeModel, PassModel, hike_pass_association
+from sqlalchemy import select, or_, insert
+
+from schemas import HikeBase
+
+
+async def create_new_hike_pass_link(
+    session: AsyncSession, hike: HikeModel, pas: PassModel
+):
+    stmt = insert(hike_pass_association).values(hike_id=hike.id, pass_id=pas.id)
+    await session.execute(stmt)
+    await session.commit()
+
+
+async def get_pass_hikes(session: AsyncSession, pass_id: int):
+    stmt = (
+        select(PassModel)
+        .options(selectinload(PassModel.hikes))  # заранее подтягиваем связанные походы
+        .where(PassModel.id == pass_id)
+    )
+    pas = await session.scalar(stmt)
+    return pas.hikes if pas else []
