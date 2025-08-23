737a396d080f7db0d11359e5e104ca52c9146472
qellyka <qellyka@proton.me>
2025-08-23 21:42:52 +0300

    Add CRUD operation for club and hike participants routes

---
+++ crud/participants.py
@@ -0,0 +1,53 @@
+from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from models import HikeModel, HikeParticipantModel, UserModel, ClubParticipantModel
+
+
+async def create_hike_participant(
+    session: AsyncSession, hike_id: int, user_id: int, role: str
+):
+    participant = HikeParticipantModel(
+        user_id=user_id,
+        hike_id=hike_id,
+        role=role,
+    )
+    session.add(participant)
+    await session.commit()
+    await session.refresh(participant)
+    return participant
+
+
+async def get_hike_participants(session: AsyncSession, hike_id: int):
+    stmt = (
+        select(HikeModel)
+        .options(
+            selectinload(HikeModel.participants).selectinload(HikeParticipantModel.user)
+        )
+        .where(HikeModel.id == hike_id)
+    )
+    hike = await session.scalar(stmt)
+    return hike.participants if hike else []
+
+
+async def get_club_participants(session: AsyncSession):
+    stmt = select(ClubParticipantModel).options(
+        selectinload(ClubParticipantModel.user)  # загружаем пользователя сразу
+    )
+    result = await session.scalars(stmt)
+    participants = result.all()  # список всех участников
+    return participants
+
+
+async def create_club_participant(
+    session: AsyncSession, user_id: int, description: str
+):
+    participant = ClubParticipantModel(
+        user_id=user_id,
+        description=description,
+    )
+    session.add(participant)
+    await session.commit()
+    await session.refresh(participant)
+    return participant
