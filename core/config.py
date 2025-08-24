26eece6dcd118bed131c64aa1758f6f57cde5fe3
qellyka <qellyka@proton.me>
2025-08-24 15:27:45 +0300

    Add settings for S3

--- core/config.py
+++ core/config.py
@@ -30,6 +30,12 @@ class Settings(BaseSettings):
     MAIL_STARTTLS: bool = True
     MAIL_SSL_TLS: bool = False
 
+    # --- S3 settings ---
+    S3_ACCESS_KEY: str
+    S3_SECRET_KEY: str
+    S3_ENDPOINT_URL: str
+    S3_BUCKET_NAME: str
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
