1fea65308933e58e14a2123c8a67296a588d7053
qellyka <qellyka@proton.me>
2025-08-26 11:25:16 +0300

    Add user routes

---
+++ api/v1/user/user.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.utils import role_required
+from crud.users import get_users, get_user_by_id
+from db import get_async_session
+from models import UserModel
+from schemas import CreateResponse, UserRead
+
+router = APIRouter(prefix="/api", tags=["User"])
+
+
+@router.get("/users", response_model=CreateResponse[List[UserRead]])
+async def get_all_users(
+    session: AsyncSession = Depends(get_async_session),
+    user: UserModel = Depends(role_required(["admin"])),
+):
+    users = await get_users(session)
+
+    return CreateResponse(
+        status="success",
+        message="ok",
+        detail=[UserRead.model_validate(user) for user in users],
+    )
+
+
+@router.get("/users/{user_id}", response_model=CreateResponse[UserRead])
+async def get_all_users(
+    user_id: int,
+    session: AsyncSession = Depends(get_async_session),
+    user: UserModel = Depends(role_required(["admin"])),
+):
+    db_user = await get_user_by_id(session, user_id)
+    return CreateResponse(
+        status="success", message="ok", detail=UserRead.model_validate(db_user)
+    )
