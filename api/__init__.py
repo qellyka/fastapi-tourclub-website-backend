5f1c5ec68eb0f69accf33fa49cdc93c3ee4f3faa
qellyka <qellyka@proton.me>
2025-08-26 11:21:49 +0300

    Update exports

--- api/__init__.py
+++ api/__init__.py
@@ -5,10 +5,13 @@ __all__ = {
     "additional_router",
     "hike_participant_router",
     "club_participant_router",
+    "user_router",
+    "auth_router",
 }
 from .v1.archive.hike import router as hike_router
 from .v1.archive.passage import router as pass_router
 from .v1.archive.additional import router as additional_router
-from .v1.user.auth import router as user_router
+from .v1.user.auth import router as auth_router
 from .v1.archive.hike_participant import router as hike_participant_router
 from .v1.user.club_participant import router as club_participant_router
+from .v1.user.user import router as user_router
