31c3c96df961224280ad9322aa8504204f317fb2
qellyka <qellyka@proton.me>
2025-08-26 11:24:55 +0300

    Add new S3 settings to support multiple buckets

--- core/config.py
+++ core/config.py
@@ -34,7 +34,8 @@ class Settings(BaseSettings):
     S3_ACCESS_KEY: str
     S3_SECRET_KEY: str
     S3_ENDPOINT_URL: str
-    S3_BUCKET_NAME: str
+    S3_HIKE_MEDIA_BUCKET_NAME: str
+    S3_USER_MEDIA_BUCKET_NAME: str
 
     class Config:
         env_file = ".env"
