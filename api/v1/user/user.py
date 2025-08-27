65b4b5700dd4b157e742460eaa7d1404cb217c73
qellyka <qellyka@proton.me>
2025-08-27 22:07:58 +0300

    Add new route

--- api/v1/user/user.py
+++ api/v1/user/user.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.utils import role_required
-from crud.users import get_users, get_user_by_id
+from crud.users import get_users, get_user_by_id, delete_user_by_id
 from db import get_async_session
 from models import UserModel
 from schemas import CreateResponse, UserRead
@@ -27,7 +27,7 @@ async def get_all_users(
 
 
 @router.get("/users/{user_id}", response_model=CreateResponse[UserRead])
-async def get_all_users(
+async def get_user(
     user_id: int,
     session: AsyncSession = Depends(get_async_session),
     user: UserModel = Depends(role_required(["admin"])),
@@ -36,3 +36,12 @@ async def get_all_users(
     return CreateResponse(
         status="success", message="ok", detail=UserRead.model_validate(db_user)
     )
+
+
+@router.delete("/users/{user_id}", status_code=204)
+async def delete_user(
+    user_id: int,
+    session: AsyncSession = Depends(get_async_session),
+    user: UserModel = Depends(role_required(["admin"])),
+):
+    deleted_user = await delete_user_by_id(session, user_id)
