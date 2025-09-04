0d6cba0f5b2e35491a6da7b99de8e9382befd849
qellyka <qellyka@proton.me>
2025-09-04 23:42:18 +0300

    Added ArticleUpdate schema for patch method

--- schemas/articles.py
+++ schemas/articles.py
@@ -16,6 +16,21 @@ class ArticleBase(BaseModel):
     author: str = Field(..., description="Автор статьи")
 
 
+class ArticleUpdate(BaseModel):
+    title: str | None = Field(..., description="Заголовок статьи")
+    slug: str | None = Field(
+        ..., description="Уникальный идентификатор статьи для URL."
+    )
+    content_json: str | None = Field(
+        ..., description="Контент статьи из Tiptap в формате JSON"
+    )
+    content_html: str | None = Field(
+        ..., description="Контент статьи из Tiptap в формате HTML"
+    )
+    cover_s3_key: str | None = Field(..., description="Обложка для статьи")
+    author: str | None = Field(..., description="Автор статьи")
+
+
 class ArticlesRead:
     title: str = Field(..., description="Заголовок статьи")
     slug: str | None = Field(
