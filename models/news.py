43313f2b3664c1cf3b401e5e8420b4507abc557e
qellyka <qellyka@proton.me>
2025-09-07 00:32:30 +0300

    Added orm model for news

---
+++ models/news.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import String, TIMESTAMP, func, Text
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import Mapped, mapped_column
+
+from . import Base
+
+
+class NewsModel(Base):
+    __tablename__ = "news"
+
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    summary: Mapped[str] = mapped_column(String(255), nullable=False)
+    slug: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+    content_json: Mapped[Optional[str]] = mapped_column(JSONB)
+    content_html: Mapped[Optional[str]] = mapped_column(Text)
+
+    cover_s3_url: Mapped[str] = mapped_column(String, nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
