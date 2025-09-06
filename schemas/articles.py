49f90ac84d13ae5937fca0869a5c3e82e6f42224
qellyka <qellyka@proton.me>
2025-09-07 00:30:33 +0300

    Update article pydantic schema

--- schemas/articles.py
+++ schemas/articles.py
@@ -1,50 +1,44 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field, ConfigDict
 
 
 class ArticleBase(BaseModel):
     title: str = Field(..., description="Заголовок статьи")
-    slug: str | None = Field(
-        ..., description="Уникальный идентификатор статьи для URL."
+    slug: Optional[str] = Field(
+        None, description="Уникальный идентификатор статьи для URL."
     )
-    content_json: str = Field(
-        ..., description="Контент статьи из Tiptap в формате JSON"
+    content_json: Optional[str] = Field(
+        None, description="Контент статьи из Tiptap в формате JSON"
     )
-    content_html: str = Field(
-        ..., description="Контент статьи из Tiptap в формате HTML"
+    content_html: Optional[str] = Field(
+        None, description="Контент статьи из Tiptap в формате HTML"
     )
-    cover_s3_key: str | None = Field(..., description="Обложка для статьи")
+    cover_s3_url: Optional[str] = Field(None, description="Обложка для статьи")
     author: str = Field(..., description="Автор статьи")
 
 
 class ArticleUpdate(BaseModel):
-    title: str | None = Field(..., description="Заголовок статьи")
-    slug: str | None = Field(
-        ..., description="Уникальный идентификатор статьи для URL."
+    title: Optional[str] = Field(None, description="Заголовок статьи")
+    slug: Optional[str] = Field(
+        None, description="Уникальный идентификатор статьи для URL."
     )
-    content_json: str | None = Field(
-        ..., description="Контент статьи из Tiptap в формате JSON"
+    content_json: Optional[str] = Field(
+        None, description="Контент статьи из Tiptap в формате JSON"
     )
-    content_html: str | None = Field(
-        ..., description="Контент статьи из Tiptap в формате HTML"
+    content_html: Optional[str] = Field(
+        None, description="Контент статьи из Tiptap в формате HTML"
     )
-    cover_s3_key: str | None = Field(..., description="Обложка для статьи")
-    author: str | None = Field(..., description="Автор статьи")
+    cover_s3_url: Optional[str] = Field(None, description="Обложка для статьи")
+    author: Optional[str] = Field(None, description="Автор статьи")
 
 
-class ArticlesRead:
-    title: str = Field(..., description="Заголовок статьи")
-    slug: str | None = Field(
-        ..., description="Уникальный идентификатор статьи для URL."
-    )
-    content_json: str = Field(
-        ..., description="Контент статьи из Tiptap в формате JSON"
-    )
-    content_html: str = Field(
-        ..., description="Контент статьи из Tiptap в формате HTML"
-    )
-    cover_s3_url: str | None = Field(..., description="Обложка для статьи")
-    author: str = Field(..., description="Автор статьи")
-
+class ArticlesRead(BaseModel):
+    id: int
+    title: str
+    slug: str
+    cover_s3_url: str
+    author: str
     model_config = ConfigDict(from_attributes=True)
 
 
