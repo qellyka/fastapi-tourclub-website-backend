bd93702696335130680c4029438d06a7d75a9b5f
qellyka <qellyka@proton.me>
2025-09-07 00:29:28 +0300

    Added CreateResponse schemas to endpoint's response

--- api/v1/article.py
+++ api/v1/article.py
@@ -1,16 +1,24 @@
 import uuid
 from pathlib import Path
+from typing import List
 
 from fastapi import APIRouter, Depends, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.config import settings
-from core.utils import role_required, generate_slug
-from crud.articles import create_new_article
+from core.utils import role_required, generate_slug, parse_article_form
+from crud.articles import (
+    create_new_article,
+    get_article_by_id,
+    update_article,
+    delete_article_by_id,
+    get_article_by_slug,
+    get_articles,
+)
 from db import get_async_session
 from models import UserModel
 from schemas import CreateResponse
-from schemas import ArticleBase
+from schemas import ArticleBase, ArticleUpdate, ArticlesRead, ArticleRead
 from services import s3_client
 
 content_type_map = {
@@ -24,64 +32,88 @@ content_type_map = {
 router = APIRouter(prefix="/api", tags=["Article"])
 
 
-@router.get("/article", response_model=CreateResponse)
-async def get_articles(
+@router.get("/articles", response_model=CreateResponse[List[ArticlesRead]])
+async def get_article_items(
     user: UserModel = Depends(role_required(["guest"])),
     session: AsyncSession = Depends(get_async_session),
 ):
-    pass
+    articles = await get_articles(session)
+    return CreateResponse(
+        status="success",
+        message="ok",
+        detail=[ArticlesRead.model_validate(article) for article in articles],
+    )
 
 
-@router.get("/article/{article_id}")
+@router.get("/articles/{slug}", response_model=CreateResponse[ArticleRead])
 async def get_article_item(
-    article_id: int,
+    slug: str,
     user: UserModel = Depends(role_required(["guest"])),
     session: AsyncSession = Depends(get_async_session),
 ):
-    pass
+    article = await get_article_by_slug(session, slug)
+
+    return CreateResponse(
+        status="success",
+        message="ok",
+        detail=ArticlesRead.model_validate(article),
+    )
 
 
-@router.post("/article")
+@router.post("/articles", response_model=CreateResponse[ArticleRead])
 async def create_new_article_item(
     cover_file: UploadFile,
-    article: ArticleBase,
+    article: ArticleBase = Depends(parse_article_form),
     user: UserModel = Depends(role_required(["admin"])),
     session: AsyncSession = Depends(get_async_session),
 ):
     extension = Path(cover_file.filename).suffix.lstrip(".")
 
     cover_bytes = await cover_file.read()
-    cover_s3_filename = f"{uuid.uuid4()}."
+    cover_s3_filename = f"{uuid.uuid4()}.{extension}"
     content_type = content_type_map.get(extension, "application/octet-stream")
 
     cover_key = await s3_client.upload_bytes(
         cover_bytes,
         cover_s3_filename,
         content_type,
-        settings.S3_USER_MEDIA_BUCKET_NAME,
+        settings.S3_ARTICLE_MEDIA_BUCKET_NAME,
     )
 
-    cover_s3_url = s3_client.object_url(cover_key)
+    cover_s3_url = s3_client.object_url(cover_key, "article-media")
 
     article.slug = generate_slug(article.title)
     article.cover_s3_url = cover_s3_url
 
-    await create_new_article(session, article)
+    article = await create_new_article(session, article)
+
+    return CreateResponse(
+        status="succes", message="ok", detail=ArticleRead.model_validate(article)
+    )
 
 
-@router.patch("/article/{article_id}")
+@router.patch("/articles/{article_id}", response_model=CreateResponse[ArticleRead])
 async def update_article_item(
     article_id: int,
+    update_data: ArticleUpdate,
     user: UserModel = Depends(role_required(["admin"])),
     session: AsyncSession = Depends(get_async_session),
 ):
-    pass
+    article_data = await get_article_by_id(session, article_id)
+
+    updated_article_data = await update_article(session, article_data, update_data)
+
+    return CreateResponse(
+        status="succes",
+        message="ok",
+        detail=ArticleRead.model_validate(updated_article_data),
+    )
 
 
-@router.delete("/article/{article_id}")
+@router.delete("/articles/{article_id}", status_code=204)
 async def delete_article_item(
     article_id: int,
     user: UserModel = Depends(role_required(["admin"])),
     session: AsyncSession = Depends(get_async_session),
 ):
-    pass
+    await delete_article_by_id(session, article_id)
